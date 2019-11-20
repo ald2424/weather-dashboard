9b77d5a81e49c1a355bf2274cfeb02202ee8eec6
ald2424 <abigaild2424@gmail.com>
2019-11-20 14:00:21 -0500

    Updated code now allows user to search one city at a time for 5 day forcast

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,14 @@
-var cityName = "Charlotte";
+// var cityName = [];
 var APIKey = "26bf907883ee024cda544990d427d76a";
-var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName +"&units=imperial&appid=" + APIKey;
-var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q="+ cityName +"&units=imperial&appid=" + APIKey;
+
 
 // This gets today's forecast
+function getTodaysForcast(city){
+
+
+var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=imperial&appid=" + APIKey;
+
+
 $.ajax({
     url: weatherURL,
     method: "GET"
@@ -15,49 +20,80 @@ $.ajax({
              var todayWind = response.wind.speed;
              var todayHumidity = response.main.humidity;
              
-             $("#displayWeather").append("City: " + city+ "<br>");
-             var day = $("<div>")
+                 $("#displayWeather").append("City: " + city+ "<br>");
+                 var day = $("<div>")
 
-                day.append( 
-                    "Today's Forecast: " + todayForecast + " " +
-                    "Wind Speed: " + todayWind + "MPH " +
-                    "Humidity: " + todayHumidity + "% "
-                    );
-                $("#displayWeather").append(day);
+                        day.append( 
+                             "Today's Forecast: " + todayForecast + " " +
+                             "Wind Speed: " + todayWind + "MPH " +
+                            "Humidity: " + todayHumidity + "% "
+                        );
+                         $("#displayWeather").append(day);
                     
-            });
-
-
-// This gets the next 4 days forecast
-$.ajax({
-    url: forecastURL,
-    method: "GET"
-})
-        // We store all of the retrieved data inside of an object called "response"
-        .then(function(response) {
-            console.log(forecastURL);
-          
-            
-        // Loops through the entire response array
-            for(var i = 0; i < response.list.length; i++){
-                // Pulls out 12:00 forecast for days 2 - 4
-                if(i == 6 ||
-                    i == 14 ||
-                    i == 22 ||
-                    i == 30){
-                var date = response.list[i].dt_txt;
-                var forecast = response.list[i].main.temp;
-                var wind = response.list[i].wind.speed;
-                var humidity = response.list[i].main.humidity;
-
-                var day = $("<div>")
-
-                day.append( 
-                    "Date: " + date + " " +
-                    "Forecast: " + forecast + "F " +
-                    "Wind Speed: " +  wind + "MPH " +
-                    "Humidity: " + humidity + "% ");
-                $("#displayWeather").append(day);
-                    }
-            }
         });
+    }
+
+
+ // This gets the next 4 days forecast
+function getFourDayForecast(city){
+
+    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q="+ city +"&units=imperial&appid=" + APIKey;
+    
+    $.ajax({
+        url: forecastURL,
+        method: "GET"
+    })
+            // We store all of the retrieved data inside of an object called "response"
+            .then(function(response) {
+                console.log(forecastURL);
+              
+                
+            // Loops through the entire response array
+                for(var i = 0; i < response.list.length; i++){
+                    // Pulls out 12:00 forecast for days 2 - 4
+                    if(i == 6 ||
+                        i == 14 ||
+                        i == 22 ||
+                        i == 30){
+                             var date = response.list[i].dt_txt;
+                             var forecast = response.list[i].main.temp;
+                             var wind = response.list[i].wind.speed;
+                             var humidity = response.list[i].main.humidity;
+    
+                                var day = $("<div>")
+    
+                                    day.append( 
+                                      "Date: " + date + " " +
+                                     "Forecast: " + forecast + "F " +
+                                     "Wind Speed: " +  wind + "MPH " +
+                                     "Humidity: " + humidity + "% ");
+                                         $("#displayWeather").append(day);
+                                }
+                 }
+            });
+        }
+
+function rendercities(){
+     $("#displayWeather").empty();
+
+        var cityInput = $("#city-input").val().trim();
+    
+        // for (var i = 0; i < cityName.length; i++) {
+            getTodaysForcast(cityInput);
+            getFourDayForecast(cityInput);
+            
+    
+    // }
+    }
+    
+    
+    $("#add-city").on("click", function(event) {
+        event.preventDefault();
+        // var input = $("#city-input").val().trim();
+        // cityName.push(input);
+        
+        rendercities();
+    });
+
+
+    
